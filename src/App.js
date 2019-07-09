8824e2028f44be6f3ae00cbadb89b6497bfb4636
Ghislain <g.soibinet@freebulle.fr>
2019-07-10 00:55:06 +0200

    Utilisation des Hooks

--- src/App.js
+++ src/App.js
@@ -1,69 +1,81 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
 import ItemCheckbox from './components/ItemCheckbox'
 
-class App extends Component {
-  state = {
-    selectedAllCheckbox: false,
-    items: [
-      { id: 1, value: "Item 1", isChecked: false },
-      { id: 2, value: "Item 2", isChecked: false },
-      { id: 3, value: "Item 3", isChecked: false },
-      { id: 4, value: "Item 4", isChecked: false }
-    ]
+const DATA = [
+  {
+    id: 1,
+    name: 'Item 1',
+    checked: false
+  },
+  {
+    id: 2,
+    name: 'Item 2',
+    checked: false
+  },
+  {
+    id: 3,
+    name: 'Item 3',
+    checked: false
+  },
+  {
+    id: 4,
+    name: 'Item 4',
+    checked: false
   }
+]
 
-  // Checkbox 'Select all'
-  handleAllChecked = (event) => {
-    let items = this.state.items
-    items.forEach(item => {       // parcours le tableau des items afin d'update la values 'isChecked' en fonction de l'event
-      item.isChecked = event.target.checked
-    })
-    this.setState({selectedAllCheckbox: event.target.checked, items: items }) //update des states
-  }
+function App() {
+  const [checkboxes, setCheckboxes] = useState(DATA)
+  const [selectAllCheckBox, setSelectAllCheckBox] = useState(false)
 
-  /*
-    Checkbox 'Item X'
-  */
-  handleItemCheck = (event) => {
-    let items = this.state.items
-    items.forEach(item => {
-        if (item.value === event.target.value)
-        item.isChecked = event.target.checked
-      }
-    )
-    
-    function isAllChecked(item) {                   // fonction test si l'item est checked
-      return (item.isChecked === true);
+  useEffect(
+    () => {
+      setSelectAllCheckBox(isAllChecked(checkboxes))
+    },
+    [checkboxes]
+  )
+
+  function isAllChecked(data) {
+    function isAllChecked(checkbox) {
+      return (checkbox.checked === true)
     }
-
-    this.setState({selectedAllCheckbox: items.every(isAllChecked) ? true : false, items: items }) //update des states
+    return data.every(isAllChecked)
   }
 
-  render() {
-    return (
-      <div className="App">
-        <input
-          type="checkbox"
-          onChange={this.handleAllChecked}            // appel de la fonction handleAllChecked à chaque changement d'état
-          checked={this.state.selectedAllCheckbox}
-        /> Select all
-        <ul>
-          {
-            this.state.items.map((item, index) => { // mapping du tableau items
-              return (
-                <ItemCheckbox                       // utilisation du component ItemCheckBox
-                  key={index}
-                  handleItemCheck={this.handleItemCheck}
-                  {...item}
-                />
-              )
-            })
-          }
-        </ul>
-      </div>
-    );
+  function handleChangeCheckBox(event, id) {
+    const isChecked = event.target.checked
+    const newData = checkboxes.map(checkbox => {
+      if (checkbox.id === id) {
+        checkbox.checked = isChecked
+      }
+      return checkbox
+    })
+    setSelectAllCheckBox(isAllChecked(newData))
+    setCheckboxes(newData)
   }
+
+  function handleChangeSelectAllCheckBox(event) {
+    const isChecked = event.target.checked
+    const newData = checkboxes.map(checkbox => {
+      checkbox.checked = isChecked
+      return checkbox
+    })
+    setSelectAllCheckBox(isChecked)
+    setCheckboxes(newData)
+  }
+
+  return (
+    <div>
+      {checkboxes.length > 1 ? <ItemCheckbox label='Select all' isChecked={selectAllCheckBox} change={(event) => handleChangeSelectAllCheckBox(event)} /> : null}
+      {
+        checkboxes.map(checkbox => (
+          <ItemCheckbox key={checkbox.id} label={checkbox.name} isChecked={checkbox.checked} change={(event) => handleChangeCheckBox(event, checkbox.id)} />
+        ))
+      }
+    </div>
+  )
 }
 
-export default App;
+export default App
