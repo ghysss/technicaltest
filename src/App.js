9e4043e7721f933e07c0afcc54623bb142ed7a2e
Ghislain <g.soibinet@freebulle.fr>
2019-07-14 19:33:33 +0200

    Update du 1er commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,144 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
 import ItemCheckbox from './components/ItemCheckbox'
 
 const DATA = [
   {
-    id: 1,
+    id: 'item-3a1a825eaa68',
     name: 'Item 1',
     checked: false
   },
   {
-    id: 2,
+    id: 'item-edc4c2026092',
     name: 'Item 2',
     checked: false
   },
   {
-    id: 3,
+    id: 'item-c16ede2ea651',
     name: 'Item 3',
     checked: false
   },
   {
-    id: 4,
+    id: 'item-f79249f54ddf',
     name: 'Item 4',
     checked: false
   }
 ]
 
-function App() {
-  const [checkboxes, setCheckboxes] = useState(DATA)
-  const [selectAllCheckBox, setSelectAllCheckBox] = useState(false)
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isCheckedList: [],
+      selectedAllCheckbox: false
+    };
+  }
 
-  useEffect(
-    () => {
-      setSelectAllCheckBox(isAllChecked(checkboxes))
-    },
-    [checkboxes]
-  )
+  /** 
+   * Chargement des datas 
+  */
+  componentDidMount() {
+    const items = [...DATA]
+    const isCheckedList = []
 
-  function isAllChecked(data) {
-    function isAllChecked(checkbox) {
-      return (checkbox.checked === true)
+    items.forEach(item => {
+      if (item.checked) {
+        isCheckedList.push(item.id);
+      }
+    })
+
+    this.setState({ items, isCheckedList, selectedAllCheckbox: items.length === isCheckedList.length });
+  }
+
+  /** 
+   * Array d'id d'items
+  */
+  listItemsId = () => {
+    const { items } = this.state;
+    const listItemsId = [];
+
+    for (const item of items) {
+      listItemsId.push(item.id);
     }
-    return data.every(isAllChecked)
+    return listItemsId;
+  };
+
+  /** 
+   * Gestion du 'Select all'
+  */
+  handleChangeSelectAllCheckBox = (event) => {
+    const { checked } = event.target;
+    let listItemsId = [];
+
+    if (checked) {
+      listItemsId = this.listItemsId();
+    }
+
+    this.setState({
+      isCheckedList: listItemsId,
+      selectedAllCheckbox: checked
+    });
   }
 
-  function handleChangeCheckBox(event, id) {
-    const isChecked = event.target.checked
-    const newData = checkboxes.map(checkbox => {
-      if (checkbox.id === id) {
-        checkbox.checked = isChecked
-      }
-      return checkbox
-    })
-    setSelectAllCheckBox(isAllChecked(newData))
-    setCheckboxes(newData)
+  /** 
+   * Gestion des items
+  */
+  handleChangeCheckBox = (event) => {
+    const { value, checked } = event.target;
+
+    if (checked) {
+      const listItemsId = this.listItemsId();
+
+      this.setState(prevState => {
+        const isCheckedList = [...prevState.isCheckedList, value]
+        return (
+          {
+            isCheckedList: isCheckedList,
+            selectedAllCheckbox: listItemsId.length === isCheckedList.length
+          }
+        )
+      });
+    } else {
+      this.setState(prevState => {
+        const isCheckedList = prevState.isCheckedList.filter(item => item !== value)
+
+        return (
+          {
+            isCheckedList: isCheckedList,
+            selectedAllCheckbox: false
+          }
+        )
+      });
+    }
   }
 
-  function handleChangeSelectAllCheckBox(event) {
-    const isChecked = event.target.checked
-    const newData = checkboxes.map(checkbox => {
-      checkbox.checked = isChecked
-      return checkbox
-    })
-    setSelectAllCheckBox(isChecked)
-    setCheckboxes(newData)
+  render() {
+    const { items, isCheckedList, selectedAllCheckbox } = this.state;
+    return (
+      <div>
+        {items.length > 1 ? <ItemCheckbox label='Select all' isChecked={selectedAllCheckbox} onChecked={this.handleChangeSelectAllCheckBox} /> : null}
+        <ul>
+          {
+            items.map(item => {
+              return (
+                <li key={item.id}>
+                  <ItemCheckbox
+                    key={item.id}
+                    id={item.id}
+                    label={item.name}
+                    onChecked={this.handleChangeCheckBox}
+                    isChecked={isCheckedList.includes(item.id)}
+                  />
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    )
   }
-
-  return (
-    <div>
-      {checkboxes.length > 1 ? <ItemCheckbox label='Select all' isChecked={selectAllCheckBox} change={(event) => handleChangeSelectAllCheckBox(event)} /> : null}
-      {
-        checkboxes.map(checkbox => (
-          <ItemCheckbox key={checkbox.id} label={checkbox.name} isChecked={checkbox.checked} change={(event) => handleChangeCheckBox(event, checkbox.id)} />
-        ))
-      }
-    </div>
-  )
 }
 
-export default App
+export default App;
--- src/components/ItemCheckbox.jsx
+++ src/components/ItemCheckbox.jsx
@@ -1,13 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 
-const ItemCheckbox = ({ label, change, isChecked }) => {
+const ItemCheckbox = ({ label, id, isChecked, onChecked }) => {
+    if (typeof id === 'undefined') {
+        id = label
+    }
+
     return (
         <Fragment>
-            <input type='checkbox' checked={isChecked} onChange={(event) => change(event)} />
-            <label>{label}</label>
-            <br />
+            <input
+                type="checkbox"
+                id={id}
+                value={id}
+                checked={isChecked}
+                onChange={onChecked}
+            />
+            <label htmlFor={id}>
+                {label}
+            </label>
         </Fragment>
-    )
+    );
 }
 
-export default ItemCheckbox
+export default ItemCheckbox;
